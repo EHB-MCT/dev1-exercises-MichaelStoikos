942aac748462f33bd2b5b6e79cb497e6ab21d030
Michael Stoikos <michael.stoikos@student.ehb.be>
2022-10-07 10:30:44 +0200

    finished 1-2 colored box

--- exercises/1-functions/1-2.js
+++ exercises/1-functions/1-2.js
@@ -1,2 +1,33 @@
 "use strict";
 
+
+drawbox();
+
+function drawbox() {
+
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    context.lineWidth = 3;
+    context.strokeStyle = 'red';
+    context.beginPath();
+    context.moveTo(50,50);
+    context.lineTo(150,150);
+    context.moveTo(150,50);
+    context.lineTo(50,150);
+    context.stroke();
+
+    context.lineWidth = 3;
+    context.strokeStyle = 'black';
+    context.beginPath();
+    context.moveTo(50,50);
+    context.lineTo(150,50);
+    context.lineTo(150,150);
+    context.lineTo(50,150);
+    context.lineTo(50,50);
+    context.stroke()
+
+    
+}

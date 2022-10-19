8fceee8049c424a343e56c413ffff9125b8443f3
Michael Stoikos <michael.stoikos@student.ehb.be>
2022-10-19 16:19:00 +0200

    finished till d

---
+++ exercises/3-Iterative/3-1-e.js
@@ -0,0 +1,24 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawDiagonalGrid();
+}
+
+function drawDiagonalGrid() {
+    let i = 0;
+    while (i <= 7) {
+        let x = ;
+        let step = 100;
+        Utils.drawLine(100, step + 50 * i, 100, step + 50 * i);
+        Utils.drawLine(step + 50 * i, 100, step + 50 * i, 100);
+        i++;
+    }
+}
